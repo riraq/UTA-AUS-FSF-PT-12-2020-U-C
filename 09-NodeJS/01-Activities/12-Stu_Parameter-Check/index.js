941f3e93e028f689cad43923b59470bf79f41748
riraq <riraq@mail.usf.edu>
2021-02-27 09:27:30 -0600

    updated activities

--- 09-NodeJS/01-Activities/12-Stu_Parameter-Check/index.js
+++ 09-NodeJS/01-Activities/12-Stu_Parameter-Check/index.js
@@ -1,4 +1,9 @@
 let argOne = process.argv[2]
 let argTwo = process.argv[3]
 
-if ()
+if (argOne === argTwo){
+    console.log("They are the same!")
+}
+else {
+    console.log("They are not the same!")
+}
--- 09-NodeJS/01-Activities/16-Stu_maths/Unsolved/index.js
+++ 09-NodeJS/01-Activities/16-Stu_maths/Unsolved/index.js
@@ -3,16 +3,37 @@ const maths = require("./maths.js")
 // console.log(maths)
 
 // TODO: Capture the values passed from the command line into these three variables: `operation`, `numOne` and `numTwo`
-if (process.argv[2] === "sum"){
-    const result = maths.sum(parseInt(process.argv[3]), parseInt(process.argv[4]))
-    console.log(result)
-}
-else if (process.argv[2] === "difference"){
-    const result = maths.difference(parseInt(process.argv[3]), parseInt(process.argv[4]))
-    console.log(result)
-}
 
+let operation = process.argv[2];
+let numOne = parseInt(process.argv[3]);
+let numTwo = parseInt(process.argv[4]);
 
 // TODO: Create a `switch` statement that accepts an `operation` parameter
 // and each `case` uses the corresponding `maths` method
 // to perform each math operation on the two numbers, `numOne` and `numTwo`
+switch(operation) {
+    case "sum":
+        console.log(maths.sum(numOne, numTwo));
+        return;
+    case "difference":
+        console.log(maths.difference(numOne, numTwo));
+        return;
+    case "product":
+        console.log(maths.product(numOne, numTwo));
+        return;
+    case "quotient":
+        console.log(maths.quotient(numOne, numTwo));
+        return;
+    default:
+        console.log("I don't recognize that command.")
+}
+
+
+// if (process.argv[2] === "sum"){
+//     const result = maths.sum(parseInt(process.argv[3]), parseInt(process.argv[4]))
+//     console.log(result)
+// }
+// else if (process.argv[2] === "difference"){
+//     const result = maths.difference(parseInt(process.argv[3]), parseInt(process.argv[4]))
+//     console.log(result)
+// }
