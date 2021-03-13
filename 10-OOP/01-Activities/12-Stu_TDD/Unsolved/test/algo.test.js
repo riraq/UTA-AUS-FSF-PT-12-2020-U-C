6f7f462f55857334e4559037de0ca02341422d96
riraq <riraq@mail.usf.edu>
2021-03-13 08:47:10 -0600

    updated activities

--- 10-OOP/01-Activities/12-Stu_TDD/Unsolved/test/algo.test.js
+++ 10-OOP/01-Activities/12-Stu_TDD/Unsolved/test/algo.test.js
@@ -14,9 +14,27 @@ describe("Algo", () => {
 
   describe("isPalindrome", () => {
     // TODO: Write a test for the `isPalindrome` method that should take a string as an argument and return the boolean `true` if the provided string is a palindrome.
+    it('should take a string as an argument and return the boolean `true` if the provided string is a palindrome'), () => {
+      const inputString = 'tacocat'
+      const obj = new Algo.isPalindrome(inputString)
+      expect(obj === inputString).toBe(true)
+    }
+
+    it('should take a string as an argument and return the boolean `false` if the provided string is not a palindrome'), () => {
+      const inputString = 'hello'
+      const obj = new Algo.isPalindrome(inputString)
+      expect(obj === inputString).toBe(false)
+    }
   });
 
   describe("capitalize", () => {
     // TODO: Write a test for the `capitalize` method that should take a string as an argument and return a new string with the first letter of each word capitalized
+    it('should take a string as an argument and return it with the first letter of each word capitalized', () => {
+      const inputString = 'hello there'
+      const capitalizedString = 'Hello There'
+      const obj = new Algo.capitalize(inputString)
+
+      expect(obj).toEqual(capitalizedString)
+    })
   });
 });
