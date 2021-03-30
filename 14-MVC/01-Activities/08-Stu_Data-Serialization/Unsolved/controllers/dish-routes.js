1c6416f4fe66853b4054cd04361574a4dba33577
riraq <riraq@mail.usf.edu>
2021-03-30 08:33:44 -0500

    updated activities

--- 14-MVC/01-Activities/08-Stu_Data-Serialization/Unsolved/controllers/dish-routes.js
+++ 14-MVC/01-Activities/08-Stu_Data-Serialization/Unsolved/controllers/dish-routes.js
@@ -4,6 +4,19 @@ const Dish = require('../models/Dish');
 
 router.get('/', async (req, res) => {
   // TODO: Build out this route so that it serializes all of the dish objects that it receives. See the 'get' route below for a hint.
+  console.log(res)
+  try{ 
+    const dishData = await Dish.findAll();
+    if(!dishData) {
+        res.status(404).json({message: 'No dish with this id!'});
+        return;
+    }
+    const dishes = dishData.map((dish) => dish.get({ plain: true }));
+    res.render('all', {dishes});
+  } catch (err) {
+      res.status(500).json(err);
+  };     
+
 });
 
 // route to get one dish
