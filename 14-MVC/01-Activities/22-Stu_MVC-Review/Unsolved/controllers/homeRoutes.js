ce5b966a62387cfd23c8d62a29f66af7a4cdee70
riraq <riraq@mail.usf.edu>
2021-04-05 13:33:04 -0500

    updated activities

--- 14-MVC/01-Activities/22-Stu_MVC-Review/Unsolved/controllers/homeRoutes.js
+++ 14-MVC/01-Activities/22-Stu_MVC-Review/Unsolved/controllers/homeRoutes.js
@@ -1,8 +1,14 @@
 const router = require('express').Router();
+const User = require('../models/User');
 
 router.get('/', async (req, res) => {
   // TODO: Render template with Sequelize data
-  res.render('homepage');
+  const userData = await User.findAll().catch((err) => { 
+    res.json(err);
+  });
+    const users = userData.map((project) => project.get({ plain: true }));
+
+  res.render('homepage', { users });
 });
 
 module.exports = router;
