144a8e000979bd5dc2600a8f159c5f8002a0b4e8
riraq <riraq@mail.usf.edu>
2021-04-26 08:38:04 -0500

    updated activities

--- 17-NoSQL/01-Activities/26-Stu-Mini-Project/Unsolved/public/db.js
+++ 17-NoSQL/01-Activities/26-Stu-Mini-Project/Unsolved/public/db.js
@@ -1,8 +1,15 @@
 let db;
+
 // create a new db request for a "BudgetDB" database.
+const request = indexedDB.open('BudgetDB', 1);
 
 request.onupgradeneeded = function (event) {
   // create object store called "BudgetStore" and set autoIncrement to true
+  const db = event.target.result;
+
+  const BudgetStore = db.createObjectStore('BudgetStore', {
+    autoIncrement: true
+  });
 };
 
 request.onsuccess = function (event) {
@@ -15,17 +22,28 @@ request.onsuccess = function (event) {
 
 request.onerror = function (event) {
   // log error here
+  console.log(event.target.errorcode)
 };
 
 function saveRecord(record) {
   // create a transaction on the pending db with readwrite access
+  const transaction = db.transaction(['BudgetStore'], 'readwrite');
+  
   // access your pending object store
+  const budgetStore = transaction.objectStore('BudgetStore');
+  
   // add record to your store with add method.
+  budgetStore.add(record)
 }
 
 function checkDatabase() {
   // open a transaction on your pending db
+  const transaction = db.transaction(['BudgetStore'], 'readwrite');
+
   // access your pending object store
+  const budgetStore = transaction.objectStore('BudgetStore');
+  
+  const getAll = BudgetStore.getAll()
   // get all records from store and set to a variable
 
   getAll.onsuccess = function () {
